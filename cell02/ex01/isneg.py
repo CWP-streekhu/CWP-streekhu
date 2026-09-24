692ba42d00d07073a40395d3de08df1ad3e02029
CWP-streekhu <66050422@kmitl.ac.th>
2026-09-24 19:36:52 +0700

    Create isneg.py

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+number = int(input())
+
+if number < 42:
+    print("This number is negative.")
+elif number > 42:
+    print("This number is positive.")
+else:
+    print("This number is both positive and negative.")
